c03c25bd230242da7ebc6f0d5326fd5dce7010b1
cuonglee <cuongle1981@gmail.com>
2013-05-07 14:08:57 +0700

    initialize app

---
+++ app/models/option.rb
@@ -0,0 +1,7 @@
+class Option < ActiveRecord::Base
+  # attr_accessible :title, :body
+  attr_accessible :content, :is_correct
+  
+  belongs_to :user
+  belongs_to :question, :class_name => 'Question', :foreign_key => 'question_id'
+end
